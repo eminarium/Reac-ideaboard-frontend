f20071d3d867172ee5d54db8bc2214cd0bc0435d
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-09-24 11:44:27 +0500

    Added addNewIdea method to add an empty idea into db

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -21,10 +21,30 @@ class IdeasContainer extends Component {
         .catch(error => console.log(error));
     }
 
+    addNewIdea = () => {
+        axios.post(
+            'http://localhost:3000/api/v1/ideas',
+            {
+                idea: {
+                    title: '',
+                    body: ''
+                }
+            }
+        )
+        .then( (response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
     render() {
         return (
             <div>
-                <button className="newIdeaButton">
+                <button className="newIdeaButton"
+                    onClick={this.addNewIdea}
+                >
                     New Idea
                 </button>
                 {
