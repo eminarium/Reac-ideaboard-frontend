8761c3759675c9b1b75264d9cbcf2b701f1f9291
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-09-25 15:35:00 +0500

    Added functionality to delete notes pressing on the 'x' in the corner

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -74,6 +74,18 @@ class IdeasContainer extends Component {
         )
     }
 
+    deleteIdea = (id) => {
+        axios.delete('http://localhost:3000/api/v1/ideas/'+id)
+        .then(response => {
+            const ideaIndex = this.state.ideas.findIndex(x => x.id === id);
+            const ideas = update(this.state.ideas, {$splice: [[ideaIndex, 1]]});
+            this.setState({ ideas: ideas});
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
     render() {
         return (
             <div>
@@ -95,10 +107,14 @@ class IdeasContainer extends Component {
                                         updateIdea={this.updateIdea}
                                         resetNotification = {this.resetNotification}
                                         titleRef = {input => this.title = input}
-                                        resetNotification = {this.resetNotification}
                                     />)                            
                         } else {
-                            return (<Idea idea={idea} key={idea.id} onClick={this.enableEditing} />);
+                            return (<Idea 
+                                        idea={idea} 
+                                        key={idea.id} 
+                                        onClick={this.enableEditing} 
+                                        onDelete={this.deleteIdea}
+                                    />);
                         }
                     })
 
